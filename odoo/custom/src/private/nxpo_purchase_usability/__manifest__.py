5c05fa84b185e611afcab2f540fa23095bb311a8
Prapassorn Sornkaew <prapassorn.s@prothaitechnology.com>
2021-01-21 04:26:56 +0000

    [ADD] nxpo_purchase_usability

---
+++ odoo/custom/src/private/nxpo_purchase_usability/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021 ProThai Technology Co.,Ltd. (http://prothaitechnology.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "NxPO - Purchase Usability",
+    "summary": "Usability improvements on purchase module",
+    "version": "14.0.1.0.0",
+    "category": "NxPO",
+    "author": "ProThai Technology Co.,Ltd.",
+    "depends": ["purchase", "purchase_request", "nxpo_base_usability"],
+    "data": [
+        "views/action_hidden_views.xml",
+        "views/purchase_views.xml",
+    ],
+    "installable": True,
+    "license": "AGPL-3",
+}
