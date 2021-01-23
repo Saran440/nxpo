9f9bb7ccd5c4e7bae8749a3a1f9b56a7210bf61d
Prapassorn Sornkaew <prapassorn.s@prothaitechnology.com>
2021-01-23 16:43:29 +0000

    [ADD] nxpo_stock_request_usability

---
+++ odoo/custom/src/private/nxpo_stock_request_usability/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2021 ProThai Technology Co.,Ltd. (http://prothaitechnology.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "NxPO - Stock Request Usability",
+    "summary": "Usability improvements on stock request module",
+    "version": "14.0.1.0.0",
+    "category": "NxPO",
+    "author": "ProThai Technology Co.,Ltd.",
+    "depends": ["stock_account", "stock_request"],
+    "data": [
+        "views/stock_request_order_views.xml",
+    ],
+    "installable": True,
+    "license": "AGPL-3",
+}
---
+++ odoo/custom/src/private/nxpo_stock_request_usability/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module adds the following functions:
+
+ - Add a smart button valuation to menu form stock request order
