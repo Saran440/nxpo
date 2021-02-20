7a78051a956925cc9fd0421ba147e9fb7343e72d
Kitti U <kittiu@gmail.com>
2021-02-20 21:27:28 +0700

    [ADD] nxpo_budget_control_excel

---
+++ odoo/custom/src/private/nxpo_budget_control_excel/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2017-19 ForgeFlow S.L. (https://www.forgeflow.com)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "NxPO - Excel for Budget Control Sheet",
+    "summary": "Import/Export Excel for Budget Control Sheet",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "NxPO",
+    "depends": ["excel_import_export", "budget_control"],
+    "external_dependencies": {"python": ["pandas", "numpy", "openpyxl"]},
+    "data": [
+        "views/actions.xml",
+        "templates/templates.xml",
+    ],
+    "installable": True,
+}
