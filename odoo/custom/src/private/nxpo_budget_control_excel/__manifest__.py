9adab747ca1d8da6fe12b2915456659a863d67be
Kitti U <kittiu@gmail.com>
2021-02-24 10:44:16 +0700

    Excel import

--- odoo/custom/src/private/nxpo_budget_control_excel/__manifest__.py
+++ odoo/custom/src/private/nxpo_budget_control_excel/__manifest__.py
@@ -7,7 +7,7 @@
     "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "category": "NxPO",
-    "depends": ["excel_import_export", "budget_control"],
+    "depends": ["excel_import_export", "budget_control", "base_name_search_improved"],
     "external_dependencies": {"python": ["pandas", "numpy", "openpyxl"]},
     "data": [
         "views/actions.xml",
